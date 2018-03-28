bed6c7473e06017176039cf76e86cf25e9046c6d
KWagner91 <kerstin-wagner@gmx.at>
2018-03-28 13:33:52 +0200

    feat: array with numbers

--- script.js
+++ script.js
@@ -33,3 +33,15 @@ function shuffle(array) {
 }
 
 
+
+
+// Make array with 2 x 8 numbers (newArray), shuffle this array
+const allCards = document.querySelectorAll(".card");
+const arrayWithNumbers = [...Array(8).keys()];
+const myArray = arrayWithNumbers.concat([...Array(8).keys()]);
+const newArray = shuffle(myArray);
+let points = 0;
+let turn = 0;
+let clickCount = 0;
+let moves = 0;
+
