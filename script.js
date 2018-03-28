cc4235a83ff572bcf2a613ae826dfabf78f02e53
KWagner91 <kerstin-wagner@gmx.at>
2018-03-28 13:22:26 +0200

    feat: Flexbox

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("test");
+// Function for making 16 cards, displayed immediately on window.onload
+function displayCards() {
+  for (let x = 0; x < 4; x++) {
+    let newTr = document.createElement("tr");
+      newTr.setAttribute("id", "row"+ x);
+    let TrElement = document.getElementById("game");
+    TrElement.appendChild(newTr);
+
+    for (let y = 0; y < 4; y++) {
+    let newTd = document.createElement("td");
+      newTd.className = "card hidden";
+    let TdElement = document.getElementById("row"+ x);
+    TdElement.appendChild(newTd);
+  }
+  }
+}
+window.onload = displayCards();
+
+
+
